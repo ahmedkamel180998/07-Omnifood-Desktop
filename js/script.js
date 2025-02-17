c7dafe3d3e9d34f1c71ef75619c034df522aa599
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-02-17 17:22:20 +0200

    add sticky navigation functionality and adjust hero section margin

--- js/script.js
+++ js/script.js
@@ -29,3 +29,21 @@ navLinks.forEach(function (link) {
     }
   });
 });
+
+const heroSection = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-100px",
+  }
+);
+obs.observe(heroSection);
