cf903698d25dbbd5f58fedd8c3e0f5fd523da691
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-02-17 14:52:45 +0200

    add mobile navigation toggle functionality and improve z-index for better visibility

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
-console.log("Hello, World!");
-
-const myName = "Ahmed Kamel";
-console.log(myName);
-
-// const h1 = document.querySelector("h1");
-const h1 = document.querySelector(".primary-heading");
-console.log(h1);
-
-h1.addEventListener("click", function () {
-  // h1.textContent = "Hello, World!";
-  h1.style.transition = "all 0.5s";
-  h1.textContent = myName;
-  h1.style.color = "red";
-  h1.style.backgroundColor = "yellow";
-  h1.style.transform = "scale(1.5)";
-});
-
 const yearSpan = document.querySelector(".current-year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
+
+const navToggle = document.querySelector(".mobile-nav-btn");
+const header = document.querySelector(".header");
+navToggle.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
